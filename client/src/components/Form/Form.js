5b5b6481935bcb5c2f568ece9e7fc02bfd79453f
Myunghoon Cha <98848704+Mcha4@users.noreply.github.com>
2022-12-03 17:06:21 -0500

    form styling

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -3,6 +3,8 @@
 import React, {useState} from "react";
 import { useDispatch } from "react-redux";
 import { createPost } from "../../actions/posts";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import './formStyle.css';
 const Form = () => {
     const [postData, setPostData] = useState({
         description: ''
@@ -23,16 +25,24 @@ const Form = () => {
         dispatch(createPost(postData));
     }
     
+    function refreshPage() {
+        window.location.reload(false);
+    }
     return (
         <>
             <h1>FORM</h1>
-            <form autoComplete="on" onSubmit={handleSubmit} method="POST">
-                <textarea name="description" onChange={(e) => setPostData({ ...postData, description: e.target.value})}></textarea>
-                {/* <CreatableSelect isClearable options={options} name="category" onChange={(e) => setPostData({ ...postData, category: e.target.value})} /> */}
-                {/* <input type="hidden" name="userID" onChange={(e) => setPostData({ ...postData, userID: e.target.value})}></input> */}
-                
-                <input type="submit" value="Submit"></input>
-            </form>
+            <div >
+                <form id="postForm" autoComplete="on" onSubmit={handleSubmit} method="POST">
+                <input className="postForm-button" onClick={refreshPage} type="submit" value="Submit"><FontAwesomeIcon icon="fa-solid fa-turn-down-left" /></input>
+                    <textarea className="postForm-textarea" name="description" onChange={(e) => setPostData({ ...postData, description: e.target.value})}></textarea>
+
+                    {/* <CreatableSelect isClearable options={options} name="category" onChange={(e) => setPostData({ ...postData, category: e.target.value})} /> */}
+                    {/* <input type="hidden" name="userID" onChange={(e) => setPostData({ ...postData, userID: e.target.value})}></input> */}
+                    
+                    
+                </form>
+            </div>
+            
         </>
         
         
